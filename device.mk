7c286564789542dd35e305bfab65bb5e7835df99
paloda <paloda09@gmail.com>
2016-07-31 23:31:17 +0200

    update f2sfs

--- device.mk
+++ device.mk
@@ -61,7 +61,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     camera.msm8916 \
     libshim_camera \
-    Snap
+    SnapdragonCamera
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -99,7 +99,12 @@ PRODUCT_PACKAGES += \
     e2fsck \
     make_ext4fs \
     setup_fs
-
+# F2FS filesystem
+PRODUCT_PACKAGES += \
+	mkfs.f2fs \
+	fsck.f2fs \
+	fibmap.f2fs \
+	f2fstat
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
